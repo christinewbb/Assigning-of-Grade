b168325c3cf9f054aca2507fe066b38aa148c49a
christinewbb <christinepalitec31@gmail.com>
2025-03-11 22:41:23 +0800

    Add files via upload

---
+++ assign_gradee.py
@@ -0,0 +1,17 @@
+def assign_grade():
+    grade = int(input("Pigaray score mo: "))
+    
+    if grade < 0 or grade > 100:
+        print("Invalid input, try again.")
+    elif grade >= 90:
+        print("grade: A")
+    elif grade >= 80:
+        print("grade: B")
+    elif grade >= 70:
+        print("grade: C")
+    elif grade >= 60:
+        print("grade: D")
+    else:
+        print("grade: F")
+        
+assign_grade()
